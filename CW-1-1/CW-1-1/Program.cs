69734cb6992f75f86ef36097732443f4697c56b8
WeebsKiller <e.3.d_x@windowslive.com>
2020-12-20 19:46:51 +0300

    Done

---
+++ CW-1-1/CW-1-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CW_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String characterName = "Becca";
+            String superPower1 = "run it";
+            String superPower2 = "cry it";
+            int age = 28;
+            double speed = 4.1;
+            Console.WriteLine("My name is {0} and my super powers are {1} and {2}" +
+                ". Im {3} years old and my speed is {4}", characterName, superPower1, superPower2, age, speed);
+        }
+    }
+}
