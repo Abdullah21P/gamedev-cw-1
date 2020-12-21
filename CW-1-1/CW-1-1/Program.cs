e3b322056e5202092a805b4ae0803a6d2cf5f73e
WeebsKiller <e.3.d_x@windowslive.com>
2020-12-21 17:41:31 +0300

    2nd task

--- CW-1-1/CW-1-1/Program.cs
+++ CW-1-1/CW-1-1/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿//Abdullah Saad M1-123
 
+using System;
 namespace CW_1_1
 {
     class Program
@@ -13,6 +14,15 @@ namespace CW_1_1
             double speed = 4.1;
             Console.WriteLine("My name is {0} and my super powers are {1} and {2}" +
                 ". Im {3} years old and my speed is {4}", characterName, superPower1, superPower2, age, speed);
+            String heroName = "Teemo";
+            double heroHeight = 1.3;
+            int heroAge = 9;
+            String heroSuperPower1 = "invis";
+            String heroSuperPower2 = "shroms";
+            int ageDifference = age - heroAge;
+            Console.WriteLine("age difference is {0}", ageDifference);
+
+
         }
     }
 }
