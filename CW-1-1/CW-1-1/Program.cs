726b7e7d2584b6f3557a1a241fcc5900028424fc
WeebsKiller <e.3.d_x@windowslive.com>
2020-12-21 18:26:21 +0300

    2nd task with the bonus

--- CW-1-1/CW-1-1/Program.cs
+++ CW-1-1/CW-1-1/Program.cs
@@ -12,6 +12,7 @@ namespace CW_1_1
             String superPower2 = "cry it";
             int age = 28;
             double speed = 4.1;
+
             Console.WriteLine("My name is {0} and my super powers are {1} and {2}" +
                 ". Im {3} years old and my speed is {4}", characterName, superPower1, superPower2, age, speed);
             String heroName = "Teemo";
@@ -21,7 +22,12 @@ namespace CW_1_1
             String heroSuperPower2 = "shroms";
             int ageDifference = age - heroAge;
             Console.WriteLine("age difference is {0}", ageDifference);
-
+            if (superPower1 == heroSuperPower1 || superPower1 == heroSuperPower2 || superPower2 == heroSuperPower2)
+            {
+                Console.WriteLine("they have the same ult");
+            }
+            else
+                Console.WriteLine("they dont have the same ult");
 
         }
     }
