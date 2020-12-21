28011c57f59d0822c78635a0ca926eebf1802dd1
WeebsKiller <e.3.d_x@windowslive.com>
2020-12-21 19:13:50 +0300

    cw2_ex3

---
+++ CW-1-1/CW-1-2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class CW-1-2
+{
+	static private double playerSpeed;
+	static void Main(string[] args)
+    {
+		SetSpeed(2.5);
+		GetSpeed();
+
+	}
+	void SetSpeed(double newSpeed) {
+		playerSpeed = newSpeed;
+	}
+	double GetSpeed() {
+		Console.WriteLine("The current player speed is {0}", playerSpeed);
+	}
+}
